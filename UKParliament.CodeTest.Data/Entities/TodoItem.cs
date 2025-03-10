e9c99a87b25739cdfc677964d5a00e4b3039c4f6
Alex Noddings <alexnoddings@users.noreply.github.com>
2025-03-10 16:50:47 +0000

    Add ToDo entity properties

--- UKParliament.CodeTest.Data/Entities/TodoItem.cs
+++ UKParliament.CodeTest.Data/Entities/TodoItem.cs
@@ -1,6 +1,18 @@
-﻿namespace UKParliament.CodeTest.Data.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UKParliament.CodeTest.Data.Entities;
 
 public class TodoItem
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
+    
+    [Required, MinLength(1), MaxLength(100)]
+    public string Title { get; set; }
+    
+    [MaxLength(500)]
+    public string? Description { get; set; }
+    
+    public bool IsCompleted { get; set; }
+    
+    public DateTime? DueDate { get; set; } 
 }
